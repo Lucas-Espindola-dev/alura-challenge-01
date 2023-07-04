0e3986f124006537a0c12d1b40efb5532aea45b9
Lucas Espíndola <128045020+Lucas-Espindola-dev@users.noreply.github.com>
2023-07-04 14:41:18 -0300

    forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
-from wtforms import StringField, SubmitField, Form
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class SearchForm(Form):
-    search = StringField('search', [DataRequired()])
-    submit = SubmitField('search',
-                         render_kw={'class': 'btn btn-sucess btn-block'})
+class SearchForm(FlaskForm):
+    searched = StringField("Searched", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
