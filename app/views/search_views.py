d85295c4991cb1d14915e23e653b1a83d3cded91
Lucas Espíndola <128045020+Lucas-Espindola-dev@users.noreply.github.com>
2023-07-04 14:42:14 -0300

    search_views.py

--- app/views/search_views.py
+++ app/views/search_views.py
@@ -1,10 +1,23 @@
-from forms import SearchForm
 from app import app
-from flask import redirect, url_for
+from forms import SearchForm
+from flask import render_template
+from ..models import video_model
+from ..entidades import video
 
 
-@app.route('/search', methods=['POST', 'GET'])
+@app.context_processor
+def base():
+    form = SearchForm()
+    return dict(form=form)
+
+
+@app.route('/search', methods=['POST'])
 def search():
-    form = SearchForm
-    if not form.validate_on_submit():
-        return redirect(url_for('index'))
+    form = SearchForm()
+    videos = video_model.Video.query
+    if form.validate_on_submit():
+        video.Video.search = form.searched.data
+        videos = videos.filter(video_model.Video.titulo)
+
+        return render_template('search.html', form=form, searched=video.Video.search,
+                               videos=videos)
